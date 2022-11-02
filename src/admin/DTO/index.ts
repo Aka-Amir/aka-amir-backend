8e31335fce159d297951e7595b7e22c5a0be5997
ArcherFsec <61965275+ArcherFsec@users.noreply.github.com>
2022-11-02 23:15:46 +0330

    Created Controller
    Created admin controller and defined admin dto

---
+++ src/admin/DTO/index.ts
@@ -0,0 +1,2 @@
+export { CreateAdminDto } from './Create.dto';
+export { UpdateAdminDto } from './Update.dto';
--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import * as dto from './DTO';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private service: AdminService) {}
+
+  @Post('create')
+  private async CreateAdmin(@Body() body: dto.CreateAdminDto) {
+    const id = await this.service.Create(body.username, body.password);
+    return {
+      message: 'created',
+      payload: id,
+    };
+  }
+
+  @Delete('remove/:id')
+  private async DeleteUser(@Param('id') id: string) {
+    const response = await this.service.Delete(id);
+    return {
+      message: 'deleted',
+      payload: response,
+    };
+  }
+
+  @Put('update')
+  private async UpdateUser(@Body() body: dto.UpdateAdminDto) {
+    const response = await this.service.UpdateUser(body.id, body.updateFields);
+    return {
+      payload: response,
+    };
+  }
+
+  @Get('id/:id')
+  private async FindAdminById(@Param('id') id: string) {
+    const response = await this.service.FindById(id);
+    return {
+      payload: response,
+    };
+  }
+
+  @Get('username/:username')
+  private async FindByUsername(@Param('username') username: string) {
+    const response = await this.service.FindByUsername(username);
+    return {
+      payload: response,
+    };
+  }
+
+  @Get('')
+  private async FindAll() {
+    const response = await this.service.FindAll();
+    return {
+      payload: response,
+    };
+  }
+}
