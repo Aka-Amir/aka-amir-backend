2db7e0462531a3c8e6057de0f7a9f2badbcda317
ArcherFsec <61965275+ArcherFsec@users.noreply.github.com>
2022-11-02 21:22:29 +0330

    updated Package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { MongooseInterceptor } from './core/interceptors/mongoose.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new MongooseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
